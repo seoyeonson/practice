76374ed3ac136f724551ba45e4dd3e1677f1b7a5
seoyeonson <eo970721@gmail.com>
2025-01-07 14:37:19 +0900

    feat : jpa post 기본 설정

---
+++ jpa2501/src/main/java/com/ll/jpa2501/post/Post.java
@@ -0,0 +1,25 @@
+package com.ll.jpa2501.post;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String username;
+    private String subject;
+    private String content;
+}
