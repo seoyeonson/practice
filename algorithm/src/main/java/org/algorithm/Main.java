e50d07c8668a0f720066951da07c496fcb083c91
seoyeonson <eo970721@gmail.com>
2025-01-15 13:53:03 +0900

    프로그래머스 Day4-2번 문제

--- algorithm/src/main/java/org/algorithm/Main.java
+++ algorithm/src/main/java/org/algorithm/Main.java
@@ -9,29 +9,20 @@ public class Main {
     public static void main(String[] args) {
 
         Solution solution = new Solution();
-        int[] result = solution.solution(1,2,3,4);
-        Arrays.stream(result).forEach(System.out::println);
+        int result = solution.solution(10);
+        System.out.println(result);
     }
 }
 
 class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        int r = 6;
 
-    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int numer = numer1*denom2 + numer2*denom1;
-        int denom = denom1*denom2;
-        int max = 1;
-
-        for(int i = 1; i<=numer && i<=denom; i++){
-            if(numer%i==0 && denom%i==0){
-                max = i;
-            }
+        while(r%n != 0){
+            answer++;
+            r = (r%n) + 6;
         }
-
-        numer /= max;
-        denom /= max;
-
-        int[] answer = {numer,denom};
-
         return answer;
     }
 }
