856754b664b593a999e9487b8ebb9bbf288add64
seoyeon <eo970721@gmail.com>
2025-07-17 13:56:39 +0900

    algo: 1463번 1로만들기 문제풀이

---
+++ algorithm/src/main/java/org/algorithm/solution/P2098_외판원순회.java
@@ -0,0 +1,19 @@
+package org.algorithm.solution;
+
+import java.io.*;
+import java.util.*;
+
+public class P2098_외판원순회 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int city = Integer.parseInt(br.readLine());
+        int current = 0;
+        int visitedCity = 0;
+
+        int[][] dp = new int[current][visitedCity];
+
+
+    }
+
+}
